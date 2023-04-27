ff7c2be5a6fa3e4a30ab0ec1379ff416c29fe3da
Nikolai Roshchin <nikolai.roshchin@gmail.com>
2023-04-27 22:25:11 +0300

    feat: add Capslock

--- virtual-keyboard/js/script.js
+++ virtual-keyboard/js/script.js
@@ -7,21 +7,34 @@ const eventCode = [
 ];
 
 const funcButtonClass = {
-	Backspace: "backspace",
-	Tab: "tab",
-	Delete: "del",
-	CapsLock: "capslock",
-	Enter: "enter",
-	Space: "space",
-	ShiftLeft: "shift-left",
-	ShiftRight: "shift-right",
+  Backspace: "backspace",
+  Tab: "tab",
+  Delete: "del",
+  CapsLock: "capslock",
+  Enter: "enter",
+  Space: "space",
+  ShiftLeft: "shift-left",
+  ShiftRight: "shift-right",
+  ControlLeft: "control-left",
+  MetaLeft: "meta-left",
+  AltLeft: "alt-left",
+  AltRight: "alt-right",
+  ControlRight: "control-right",
 };
 
-const english = [
+const englishLower = [
   ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
   ["Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "Del"],
   ["CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\", "Enter"],
   ["Shift", "\\", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "▲", "Shift"],
+  ["Ctrl", "Win", "Alt", " ", "Alt Gr", "◄", "▼", "►", "Ctrl"],
+];
+
+const englishUpper = [
+  ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Backspace"],
+  ["Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "Del"],
+  ["CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "|", "Enter"],
+  ["Shift", "|", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?", "▲", "Shift"],
   ["Ctrl", "Win", "Alt", " ", "Alt Gr", "◄", "▼", "►", "Ctrl"],
 ];
 
@@ -36,20 +49,89 @@ textarea.className = "textarea";
 
 let keyboard = document.createElement("div");
 keyboard.className = "keyboard";
+keyboard.id = "keyboard";
+
+let isCapsLock = false;
 
 body.appendChild(container);
 container.appendChild(textarea);
 container.appendChild(keyboard);
 
 
-for(let i = 0; i < english.length; i++) {
+for(let i = 0; i < englishLower.length; i++) {
   let keyboardRow = keyboard.appendChild(document.createElement("div"));
   keyboardRow.className = "keyboard__row";
-  for(let j = 0; j < english[i].length; j++) {
-    let key = keyboardRow.appendChild(document.createElement("button"));
-    key.innerHTML = english[i][j];
-    key.id = eventCode[i][j];
-    key.className = "button";
-    if (key.id in funcButtonClass) key.classList.add(funcButtonClass[key.id]);
+  for(let j = 0; j < englishLower[i].length; j++) {
+    let button = keyboardRow.appendChild(document.createElement("button"));
+    button.innerHTML = englishLower[i][j];
+    button.id = eventCode[i][j];
+    button.className = "button";
+    if (button.id in funcButtonClass) button.classList.add(funcButtonClass[button.id]);
+    button.addEventListener('click', inputText);
+  }
+}
+
+function inputText(e) {
+  let cursorPosition = textarea.selectionStart;
+  switch (e.target.id) {
+    case "Backspace":
+  	  deleteSymbol(cursorPosition - 1);
+  	  break;
+    case "Delete":
+      deleteSymbol(cursorPosition);
+  	  break;
+  	case "Tab":
+  	  insertSymbol("    ", cursorPosition);
+  	  break;
+  	case "Enter":
+  	  insertSymbol("\n", cursorPosition);
+  	  break;
+  	default:
+      if (!(e.target.id in funcButtonClass) || (e.target.id === "Space")) {
+        insertSymbol(e.target.innerHTML, cursorPosition);
+  	  }
+  }
+}
+
+function deleteSymbol(position) {
+	textarea.value =
+  		  textarea.value.substring(0, position) +
+  	      textarea.value.substring(position + 1);
+  	      textarea.focus();
+  	      textarea.selectionStart = position;
+  	      textarea.selectionEnd = position;
+}
+
+function insertSymbol(symbol, position) {
+  textarea.value =
+        textarea.value.slice(0, position) +
+        symbol +
+  	    textarea.value.slice(position);
+  	    textarea.focus();
+  	    textarea.selectionEnd = position + symbol.length;
+}
+
+let capslock = document.querySelector("#CapsLock");
+capslock.addEventListener("click", toggleCapsLock);
+
+function toggleCapsLock() {
+  isCapsLock = !isCapsLock;
+  changeCase();
+}
+
+function changeCase() {
+  let i = 0;
+  let j = 0;
+  for (const el of keyboard.children) {
+  	for (const keyS of el.children) {
+      if (isCapsLock) {
+      	keyS.innerHTML = englishUpper[i][j];
+      } else {
+        keyS.innerHTML = englishLower[i][j];
+      }
+      j++
+    }
+    j = 0;
+    i++;
   }
 }
